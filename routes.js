c00c87617ef48c2ddc5c5eabf539ec12125332cc
dantesantos <46685889+dantesantos@users.noreply.github.com>
2019-06-20 13:40:13 -0400

    new changes

--- routes.js
+++ routes.js
@@ -3,9 +3,11 @@ const app = express();
 
 //Import our pages routes
 const pageRoutes = require('./routes/pages');
+const blogsRoutes = require('./routes/blogs');
 
 //Register our page routes
 app.use('/', pageRoutes);
+app.use('/blogs', blogsRoutes);
 
 //Export the changes
 module.exports = app;
---
+++ routes/blogs.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const BlogsController = require('../controllers/blogsController');
+
+//Begin routes
+//this user wants to do this...
+router.get('/', BlogsController.index);
+router.get('/new', BlogsController.new);
+router.get('/:id', BlogsController.show);
+router.get('/:id/edit', BlogsController.edit);
+router.post('/', BlogsController.create);
+router.post('/update', BlogsController.update);
+router.post('/destroy', BlogsController.destroy);
+
+//End routes
+
+module.exports = router;
